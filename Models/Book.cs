69b51c950784faa2c484c680a6528ebd1698e5e8
Oli Goodall <Oli.Goodall@softwire.com>
2022-09-23 11:02:56 +0100

    BK: 00 Bookish database set up

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,10 +2,11 @@ namespace Bookish.Models
 {
     public class Book
     {
-        public string Title { get; set;}
-        public string Author { get; set; }
-        public string CoverImageUrl { get; set; }
-        public string Blurb { get; set; }
+        public int Id { get; set; }
+        public string? Title { get; set;}
+        public string? Author { get; set; }
+        public string? CoverImageUrl { get; set; }
+        public string? Blurb { get; set; }
 
         // public Book(string title, string author, string coverImageUrl, string blurb)
         // {
